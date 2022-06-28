89e66aee7bdb16236c0b5cd4bb242bc55045d2ae
Nocturnal23 <matarazzogennaro1997@gmail.com>
2022-06-28 19:50:44 +0200

    Added not found page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HelloworldComponent} from "./helloworld/helloworld.component";
+import {NotFoundComponent} from "./layout/not-found/not-found.component";
 
 const routes: Routes = [
-  { path: 'helloworld', component: HelloworldComponent}
+  { path: 'helloworld', component: HelloworldComponent },
+  { path: '**', pathMatch: 'full', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ContainerComponent } from './layout/container/container.component';
 import {RouterModule} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { HelloworldComponent } from './helloworld/helloworld.component';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    HelloworldComponent
+    HelloworldComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
