0d33bb1e89bcd037adcdf77b9d4d1ce9897dc86b
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-04-19 18:38:32 +0200

    Messing stuff up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import {RouterModule, Routes} from "@angular/router";
 import {HelloworldComponent} from "./helloworld/helloworld.component";
 import {NotFoundComponent} from "./layout/not-found/not-found.component";
 import {LoginpageComponent} from "./layout/loginpage/loginpage.component";
+import {ContainerComponent} from "./layout/container/container.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
 const routes: Routes = [
-  { path: 'helloworld', component: HelloworldComponent },
+  { path: 'not_found', component: NotFoundComponent },
   { path: 'login', component:LoginpageComponent },
-  { path: '**', pathMatch: 'full', component: NotFoundComponent }
+  { path: '', component: ContainerComponent, children: [
+      { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+      { path: 'homepage', component: HomepageComponent },
+      { path: 'helloworld', component: HelloworldComponent },
+      { path: '**', redirectTo: 'not_found', pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
