dabb8a0f8ad8e70132ca505c41ad27c72a405a8a
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-05-02 17:54:56 +0200

    Refactor event files.

--- src/app/core/criteria/advert-criteria.ts
+++
@@ -1,5 +0,0 @@
-import {BaseCriteria} from "./base-criteria";
-
-export interface AdvertCriteria extends BaseCriteria {
-
-}
--- src/app/core/services/event.service.ts
+++ src/app/core/services/event.service.ts
@@ -1,26 +1,26 @@
 import {Injectable} from "@angular/core";
 import {AbstractService} from "./abstract.service";
-import {AdvertDto} from "../dto/advert-dto";
-import {AdvertCriteria} from "../criteria/advert-criteria";
+import {EventDto} from "../dto/event-dto";
+import {EventCriteria} from "../criteria/event-criteria";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Page} from "../commons/filter.response";
 
 @Injectable({ providedIn: 'root' })
-export class AdvertService implements AbstractService<AdvertDto, AdvertCriteria> {
+export class EventService implements AbstractService<EventDto, EventCriteria> {
     private baseUrl: string;
 
     constructor(private _http: HttpClient) {
         this.baseUrl = '/adverts'
     }
 
-    get(id: number | string): Observable<AdvertDto> {
+    get(id: number | string): Observable<EventDto> {
         const url = this.baseUrl + '/' + id;
-        return this._http.get<AdvertDto>(url);
+        return this._http.get<EventDto>(url);
     }
 
-    filter(criteria: AdvertCriteria): Observable<Page<AdvertDto>> {
+    filter(criteria: EventCriteria): Observable<Page<EventDto>> {
         const url = this.baseUrl + '/filter';
-        return this._http.post<Page<AdvertDto>>(url, criteria);
+        return this._http.post<Page<EventDto>>(url, criteria);
     }
 }
