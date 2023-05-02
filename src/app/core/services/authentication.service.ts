06aab2a417c8e91d2db9024bbfd838a8e4c3ec2e
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-05-02 18:03:46 +0200

    Added form group and signUp method

---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@angular/core";
+import {UserDto} from "../dto/user-dto";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Injectable({providedIn: "root"})
+export class AuthenticationService {
+  private baseUrl : string
+  constructor( private _http: HttpClient) {
+    this.baseUrl = environment.BE_URL + "/v1"
+  }
+
+  signUp(user : UserDto) : Observable<UserDto> {
+    const url = this.baseUrl + "/signUp"
+    return this._http.post<UserDto>(url, user)
+  }
+}
--- src/app/layout/loginpage/loginpage.component.ts
+++ src/app/layout/loginpage/loginpage.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../core/services/authentication.service";
+import {UserDto} from "../../core/dto/user-dto";
 
 @Component({
   selector: 'app-loginpage',
@@ -9,11 +13,17 @@ export class LoginpageComponent implements OnInit {
 
 
   container: any;
+  signUpForm: FormGroup;
   login: boolean;
 
   constructor(private _formBuilder: FormBuilder,
               private _authService: AuthenticationService) {
-
+    this.signUpForm = _formBuilder.group({
+      firstname: [''],
+      lastname: [''],
+      email: [''],
+      password: ['']
+    })
     this.login = true
   }
   ngOnInit() {
@@ -30,5 +40,12 @@ export class LoginpageComponent implements OnInit {
       this.container.classList.remove("right-panel-active");
       this.login = true;
     }
+  }
+
+  signUp() {
+    const user : UserDto = {
+      ...this.signUpForm.value
+    }
+    this._authService.signUp(user).subscribe()
   }
 }
