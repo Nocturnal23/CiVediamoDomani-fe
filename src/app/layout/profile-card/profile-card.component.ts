8f9bca92483ca320359548bb4273a5e52326baef
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-05-21 16:42:01 +0200

    Added logout button in profile card

--- src/app/layout/profile-card/profile-card.component.ts
+++ src/app/layout/profile-card/profile-card.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {AuthenticationService} from "../../core/services/authentication.service";
 
 @Component({
-  selector: 'app-profile-card',
-  templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.css']
+    selector: 'app-profile-card',
+    templateUrl: './profile-card.component.html',
+    styleUrls: ['./profile-card.component.css']
 })
 export class ProfileCardComponent {
 
+    constructor(private _authService : AuthenticationService) {
+    }
+    logout() {
+        this._authService.logOut();
+    }
 }
