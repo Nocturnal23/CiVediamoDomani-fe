8284c070b7e44f18808febe385723a9ffaf20a39
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-05-26 16:26:21 +0200

    Search page now shows data from db

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,48 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {EventService} from "../../core/services/event.service";
-import {UserService} from "../../core/services/user.service";
-import {CategoryService} from "../../core/services/category.service";
+import {EventDto} from "../../core/dto/event-dto";
+import {map, Observable} from "rxjs";
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+    selector: 'app-search-page',
+    templateUrl: './search-page.component.html',
+    styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit{
-  public searchLocation: string = '';
-  public searchRange: number = 0;
-  public sliderMinRange: number = 10;
-  public sliderMaxRange: number = 100;
-  public sliderStep: number = 10;
-  public values: any = '';
-  public stampa: any;
-  public stampaUser: any;
-  public stampaCategory: any;
+    public searchLocation: string = '';
+    public searchRange: number = 0;
+    public sliderMinRange: number = 10;
+    public sliderMaxRange: number = 100;
+    public sliderStep: number = 10;
+    public values: any = '';
+    // @ts-ignore
+    eventList$: Observable<EventDto[]>;
 
-  constructor(private router: Router,
-              private activatedRoute: ActivatedRoute,
-              private service: EventService,
-              private userService: UserService,
-              private categoryService: CategoryService
-  ) {
+    constructor(private router: Router,
+                private activatedRoute: ActivatedRoute,
+                private eventService: EventService) {
+    }
+    ngOnInit() {
+        this.activatedRoute.params.subscribe( routeParams => {this._loadData(routeParams)} )
+        this.searchLocation = 'Milano';
+    }
 
-    this.stampa = service.filter({}).subscribe()
-    console.log(this.stampa)
+    private _loadData( routeParams : any ) {
+        this.values = routeParams['query']
+        // @ts-ignore
+        this.eventList$ = this.eventService.filter({}).pipe(map(res => res.content));
 
-    this.stampaUser = userService.filter({}).subscribe()
-    console.log(this.stampaUser)
-
-    this.stampaCategory = categoryService.filter({}).subscribe()
-    console.log(this.stampaCategory)
-  }
-  ngOnInit() {
-    this.activatedRoute.params.subscribe( routeParams => {this._loadData(routeParams)} )
-    this.searchLocation = 'Milano';
-  }
-
-  private _loadData( routeParams : any ) {
-    this.values = routeParams['query']
-    console.log("Da search-page: " + this.values)
-  }
+    }
 }
