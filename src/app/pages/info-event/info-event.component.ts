fa873425b17986e8fa1100aa29d47afb86f1aa2b
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-05-29 17:56:15 +0200

    Added event info page.

---
+++ src/app/pages/info-event/info-event.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {EventDto} from "../../core/dto/event-dto";
+
+@Component({
+    selector: 'app-info-event',
+    templateUrl: './info-event.component.html',
+    styleUrls: ['./info-event.component.css']
+})
+export class InfoEventComponent {
+
+    eventDto: EventDto;
+
+    constructor() {
+        this.eventDto = {
+            id: 0,
+            title : "Titolo evento",
+            place : "Luogo evento",
+            dateTime : new Date(2000,1,1),
+            description : "Descrizione evento",
+            price : 0,
+            categories : [{name : "sport", id : 0}, {name : "calcio", id : 1}]
+        }
+    }
+
+    isFree() {
+        return true;
+    }
+}
