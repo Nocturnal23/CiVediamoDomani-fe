78fca99facb8b18a805547e82db91953d1eaf0ee
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-06-06 19:25:35 +0200

    dashboard user load users data.

--- src/app/pages/dashboard-user/dashboard-user.component.ts
+++ src/app/pages/dashboard-user/dashboard-user.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {UserDto} from "../../core/dto/user-dto";
+import {UserService} from "../../core/services/user.service";
+import {map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-user',
@@ -7,21 +10,17 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./dashboard-user.component.css']
 })
 export class DashboardUserComponent implements OnInit {
-  nome: string;
-  cognome: string;
-  tipo_utente: string;
-  eventi_creati: number;
-  gestisci: string;
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.nome = "nome1"
-    this.cognome = "cognome1"
-    this.tipo_utente = "tipo1"
-    this.eventi_creati = 1
-    this.gestisci = "gestisci"
+  userData: Observable<UserDto[]> | undefined
+  constructor(private activatedRoute: ActivatedRoute,
+              private userService: UserService) {
   }
 
   ngOnInit() {
-  }
+    this.activatedRoute.params.subscribe( routeParams => {
 
+      // @ts-ignore
+      this.userData = this.userService.filter({}).pipe(map(res => res.content));
+    })
+  }
 }
