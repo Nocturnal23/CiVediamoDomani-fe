c07ef912d879a3731d2bf44691fec7ab5bd893d8
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-07 18:09:31 +0200

    Adjusting search page

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {EventService} from "../../core/services/event.service";
 import {EventDto} from "../../core/dto/event-dto";
+import {CategoryService} from "../../core/services/category.service";
 
 @Component({
     selector: 'app-search-page',
@@ -9,26 +10,23 @@ import {EventDto} from "../../core/dto/event-dto";
     styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit{
-    public searchLocation: string = '';
-    public searchRange: number = 0;
-    public sliderMinRange: number = 10;
-    public sliderMaxRange: number = 100;
-    public sliderStep: number = 10;
-    public values: any = '';
+    searchRange: number = 0;
+    sliderMinRange: number = 10;
+    sliderMaxRange: number = 100;
+    sliderStep: number = 10;
+
+    searchValue: any = '';
+    searchLocation: string = '';
     eventList: EventDto[] = [];
 
-    constructor(private router: Router,
-                private activatedRoute: ActivatedRoute,
-                private eventService: EventService) {
+    constructor(private _router: Router,
+                private _activatedRoute: ActivatedRoute,
+                private _eventService: EventService,
+                private _categoryService: CategoryService) {
     }
     ngOnInit() {
-        this.activatedRoute.data.subscribe(
-            ({eventList}) => {
-                this.eventList = eventList.content
-            });
-        this.activatedRoute.params.subscribe(({query}) => {
-            this.values = query
-        })
+        this._activatedRoute.data.subscribe(({eventList}) => { this.eventList = eventList.content });
+        this._activatedRoute.params.subscribe(({query}) => { this.searchValue = query });
         this.searchLocation = 'Milano';
     }
 }
