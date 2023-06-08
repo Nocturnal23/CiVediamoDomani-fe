ee2c26ce1b6db52d951cf3a04ae49f1e36b84586
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-08 18:07:24 +0200

    Adjusted indentation in dashboard user

--- src/app/pages/dashboard-user/dashboard-user.component.ts
+++ src/app/pages/dashboard-user/dashboard-user.component.ts
@@ -7,29 +7,29 @@ import {Page} from "../../core/commons/filter.response";
 import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
-  selector: 'app-dashboard-user',
-  templateUrl: './dashboard-user.component.html',
-  styleUrls: ['./dashboard-user.component.css']
+    selector: 'app-dashboard-user',
+    templateUrl: './dashboard-user.component.html',
+    styleUrls: ['./dashboard-user.component.css']
 })
 export class DashboardUserComponent implements OnInit {
 
-  userLenght: number = 0
-  userPageSize: number = 0
-  userInter: UserDto[] = []
-  displayedColumns: string[] = ["nome", "cognome", "tipoUtente", "eventiCreati"]
-  @ViewChild(MatPaginator, {static: true})
-  paginator: MatPaginator | undefined
+    userLenght: number = 0
+    userPageSize: number = 0
+    userInter: UserDto[] = []
+    displayedColumns: string[] = ["nome", "cognome", "tipoUtente", "eventiCreati"]
+    @ViewChild(MatPaginator, {static: true})
+    paginator: MatPaginator | undefined
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private userService: UserService) {
-  }
+    constructor(private activatedRoute: ActivatedRoute,
+                private userService: UserService) {
+    }
 
-  ngOnInit() {
-      this.activatedRoute.data.subscribe(
-        ({userList}) => {
-          this.userInter = userList.content
-          this.userLenght = userList.totalElements
-          this.userPageSize = userList.pageSize
-        });
+    ngOnInit() {
+        this.activatedRoute.data.subscribe(
+            ({userList}) => {
+                this.userInter = userList.content
+                this.userLenght = userList.totalElements
+                this.userPageSize = userList.pageSize
+            });
     }
 }
