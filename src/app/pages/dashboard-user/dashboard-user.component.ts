2cdb12d36bbdc8e06cf525da3c31ba02d6a16657
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-06-08 20:27:19 +0200

    Adjusting table.

--- src/app/pages/dashboard-user/dashboard-user.component.ts
+++ src/app/pages/dashboard-user/dashboard-user.component.ts
@@ -5,6 +5,7 @@ import {UserService} from "../../core/services/user.service";
 import {map, Observable} from "rxjs";
 import {Page} from "../../core/commons/filter.response";
 import {MatPaginator} from "@angular/material/paginator";
+import {Column, LazyLoadEvent} from "../../layout/table";
 
 @Component({
     selector: 'app-dashboard-user',
@@ -13,23 +14,40 @@ import {MatPaginator} from "@angular/material/paginator";
 })
 export class DashboardUserComponent implements OnInit {
 
-    userLenght: number = 0
-    userPageSize: number = 0
-    userInter: UserDto[] = []
-    displayedColumns: string[] = ["nome", "cognome", "tipoUtente", "eventiCreati"]
-    @ViewChild(MatPaginator, {static: true})
-    paginator: MatPaginator | undefined
+    userMock: UserDto[] = [ { id:1, email:'ciccio', firstName:'ciccio', lastName:'ciccio', appRole:2, organisedEvents:5 } ]
+
+    userLenght: number
+    userPageSize: number = 5
+    userInter: Observable<Array<UserDto>>
+    displayedColumns: Column[] = [
+      { name: 'Nome', id: 'firstName', sort: 'firstName', path: 'firstName',  visible: true, isModelProperty: true },
+      { name: 'Cognome', id: 'lastName', sort: 'lastName' , path: 'lastName', visible: true, isModelProperty: true },
+      { name: 'Tipo Utente', id: 'appRole', sort: 'appRole' , path: 'appRole', visible: true, isModelProperty: true },
+      { name: 'Eventi Creati', id: 'organisedEvents', sort: 'organisedEvents' , path: 'organisedEvents', visible: true, isModelProperty: true }
+    ]
 
     constructor(private activatedRoute: ActivatedRoute,
                 private userService: UserService) {
     }
 
     ngOnInit() {
-        this.activatedRoute.data.subscribe(
-            ({userList}) => {
-                this.userInter = userList.content
-                this.userLenght = userList.totalElements
-                this.userPageSize = userList.pageSize
-            });
+        // this.userInter = this.activatedRoute.data.pipe(
+        //   map( res => {
+        //       this.userLenght = res['userList'].totalElements
+        //       // console.log(this.userLenght)
+        //       // this.userPageSize = res['userList'].pageSize
+        //     return res['userList'].content
+        //   })
+        // );
+        // this.userInter.subscribe(() => console.log(this.userInter) )
+        this.userInter = this.userService.filter({}).pipe(
+          map( res => { return res.content } )
+        )
+    }
+
+    doLazyLoad( event: LazyLoadEvent ) {
+      this.userInter = this.userService.filter({}).pipe(
+        map( res => { return res.content } )
+      )
     }
 }
