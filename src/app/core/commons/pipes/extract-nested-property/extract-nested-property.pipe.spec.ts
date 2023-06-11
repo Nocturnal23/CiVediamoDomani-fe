269da07b47a9a1f93ff6471805f4908d6d67dcfc
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-11 21:23:22 +0200

    Deleted useless file

--- src/app/core/commons/pipes/extract-nested-property/extract-nested-property.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExtractNestedPropertyPipe } from './extract-nested-property.pipe';
-
-describe('Pipe: ExtractNestedPropertye', () => {
-  it('create an instance', () => {
-    const pipe = new ExtractNestedPropertyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
