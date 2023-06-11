1964a68fa1a90b24c4bf56443dd7267a1615fc84
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-11 18:15:21 +0200

    Adjusted nested property pipe to show empty values

--- src/app/core/commons/pipes/extract-nested-property/extract-nested-property.pipe.ts
+++ src/app/core/commons/pipes/extract-nested-property/extract-nested-property.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'extractNestedProperty'
+    name: 'extractNestedProperty'
 })
 export class ExtractNestedPropertyPipe implements PipeTransform {
 
-  transform(value: any, path: string): any {
-    return path.split('.').reduce((previous, current) =>  previous[current] || '', value);
-  }
+    transform(value: any, path: string): any {
+        return path.split('.').reduce((previous, current) =>  {
+            const prop = previous[current];
+            return prop !== undefined && prop !== null ? prop : '';
+        }, value);
+    }
 
 } 
