454bab6a65d90971b7f0f209cec52bc143f66330
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-15 00:54:42 +0200

    Minor code adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HelloworldComponent} from "./helloworld/helloworld.component";
 import {NotFoundComponent} from "./layout/not-found/not-found.component";
--- src/app/layout/left-menu-dashboard/left-menu-dashboard.component.ts
+++ src/app/layout/left-menu-dashboard/left-menu-dashboard.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-left-menu-dashboard',
-  templateUrl: './left-menu-dashboard.component.html',
-  styleUrls: ['./left-menu-dashboard.component.css']
+    selector: 'app-left-menu-dashboard',
+    templateUrl: './left-menu-dashboard.component.html',
+    styleUrls: ['./left-menu-dashboard.component.css']
 })
 export class LeftMenuDashboardComponent {
 
