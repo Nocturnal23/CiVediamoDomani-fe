39780fe153cb54f4d07e1f68786e0a49b13ede17
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-14 20:13:52 +0200

    Added url enums

--- src/app/core/utils/Enums.ts
+++ src/app/core/utils/Enums.ts
@@ -1,4 +1,8 @@
 export enum Enums {
     ADMIN_ROLE = 1,
-    NORMAL_ROLE = 2
+    NORMAL_ROLE = 2,
+
+    USER_PROFILE = '/user/profile',
+    USER_EVENTS = '/user/events',
+    USER_SETTINGS = '/user/settings'
 }
