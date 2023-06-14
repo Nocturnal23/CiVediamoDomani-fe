e2ad3e7dabe7ffcdca1204119d41f6ca3b42eadd
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-06-14 21:37:26 +0200

    Adjusted profile page data loading.

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {AuthenticationService} from "../../core/services/authentication.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {UserDto} from "../../core/dto/user-dto";
+import {UserService} from "../../core/services/user.service";
+import {AuthenticationService} from "../../core/services/authentication.service";
 
 @Component({
     selector: 'app-profile-page',
@@ -9,16 +10,22 @@ import {UserDto} from "../../core/dto/user-dto";
     styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent {
-    name: string | undefined
-    surname: string | undefined
-    email: string | undefined
-    password: string | undefined
-    user: UserDto
+    user: UserDto = AuthenticationService.getAppUser
     constructor(private router: Router,
-                private activatedRoute: ActivatedRoute) {
-        this.user = this.router.getCurrentNavigation().extras.state['user']
+                private activatedRoute: ActivatedRoute,
+                private userService: UserService) {
+        this.user = this.router.getCurrentNavigation()?.extras?.state?.['user']
+
         if (!this.user) {
-            this.activatedRoute.params.subscribe(({url}) => { this.user });
+
+            this.activatedRoute.params.subscribe(({url}) => {
+                if ( url === AuthenticationService.getAppUser?.url ) {
+                    this.user = AuthenticationService.getAppUser
+                }
+                else {
+                    this.userService.getByUrl(url).subscribe(user => this.user = user)
+                }
+            });
         }
     }
 }
