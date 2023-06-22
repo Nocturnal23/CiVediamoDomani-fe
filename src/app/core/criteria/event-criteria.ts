d956a6d939e79f7907e79e58a502ec935ca2e934
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-06-22 17:17:12 +0200

    Added endpoints.

--- src/app/core/criteria/event-criteria.ts
+++ src/app/core/criteria/event-criteria.ts
@@ -2,4 +2,7 @@ import {BaseCriteria} from "./base-criteria";
 
 export interface EventCriteria extends BaseCriteria {
     searchValue?: string
+    organiserId?: number
+    followerId?: number
+    attendeeId?: number
 }
