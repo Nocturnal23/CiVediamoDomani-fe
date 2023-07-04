af430e3527600a4aeccd6d97ca2b59e6bb8b77aa
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-07-04 19:56:48 +0200

    Added carousel in homepage.

--- src/app/pages/homepage/home-page.component.ts
+++ src/app/pages/homepage/home-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import {AuthenticationService} from "../../core/services/authentication.service";
+import {CategoryService} from "../../core/services/category.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {map, Observable, Subscription} from "rxjs";
+import {CategoryDto} from "../../core/dto/category-dto";
+import {EventService} from "../../core/services/event.service";
 
 @Component({
     selector: 'app-homepage',
@@ -7,12 +12,49 @@ import {AuthenticationService} from "../../core/services/authentication.service"
     styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+    categories: Observable<Array<CategoryDto>>
+    categorieCarosello: CategoryDto[] = [];
+    indx = 0
+    currentIndx = 0
 
-    constructor(private _authService : AuthenticationService) {
-        console.log('Homepage: ' + AuthenticationService.getAuthToken)
+
+    constructor(private _authService : AuthenticationService,
+                private _categoryService: CategoryService,
+                private _eventService: EventService,
+                private _router: Router,
+                private _activatedRoute: ActivatedRoute) {
+
+        this.categories = this._categoryService.filter({}).pipe(
+            map((macroCategories) => macroCategories.content)
+        );
+
+        this.categories.subscribe((categoryArray: CategoryDto[]) => {
+            categoryArray.forEach((category: CategoryDto) => {
+                if( category.father == null ) {
+                    this.categorieCarosello.push(category)
+                }
+            });
+        });
     }
 
     showCreateButton() : boolean {
         return this._authService.isLogged();
+    }
+
+    changeCategory(move: number) {
+        let categoria
+        if( move == 1 ) {
+            categoria = this.categorieCarosello.shift()
+            this.categorieCarosello.push(categoria)
+        }
+
+        if( move == -1 ) {
+            categoria = this.categorieCarosello.pop()
+            this.categorieCarosello.unshift(categoria)
+        }
+    }
+
+    loadEvents(id: number) {
+        console.log(id)
     }
 }
