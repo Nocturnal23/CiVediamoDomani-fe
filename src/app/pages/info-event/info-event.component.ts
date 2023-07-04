2e244f30673b96d772dea48ea0a286894eea3ff8
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-07-04 21:04:19 +0200

    Adjusted infoevent graphic.

--- src/app/pages/info-event/info-event.component.ts
+++ src/app/pages/info-event/info-event.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {EventDto} from "../../core/dto/event-dto";
 import {EventService} from "../../core/services/event.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {AuthenticationService} from "../../core/services/authentication.service";
 
 @Component({
     selector: 'app-info-event',
@@ -11,6 +12,9 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class InfoEventComponent {
     eventDto: EventDto;
     url: string
+    eventDate: string;
+    attending: boolean;
+    favorite: boolean;
 
     constructor(private eventService: EventService,
                 private router: Router,
@@ -18,12 +22,18 @@ export class InfoEventComponent {
 
         this.eventDto = this.router.getCurrentNavigation()?.extras?.state?.['event']
         if (this.eventDto) {
+            this.eventDate = new Date(this.eventDto?.datetime).toLocaleString()
+            this.attending = this.eventDto.attendees.includes(AuthenticationService.getAppUser.url)
+            this.favorite = this.eventDto.followers.includes(AuthenticationService.getAppUser.url)
             this.isFree()
         } else {
             this.activatdRoute.params.subscribe(({url}) => {
                 this.url = url;
                 this.eventService.getByUrl(this.url).subscribe((event) => {
                     this.eventDto = event;
+                    this.eventDate = new Date(this.eventDto?.datetime).toLocaleString()
+                    this.attending = this.eventDto.attendees.includes(AuthenticationService.getAppUser.url)
+                    this.favorite = this.eventDto.followers.includes(AuthenticationService.getAppUser.url)
                     this.isFree()
                 });
             });
@@ -36,5 +46,9 @@ export class InfoEventComponent {
 
     isFree() {
         return this.eventDto?.price == 0;
+    }
+
+    onGoin() {
+        return new Date(this.eventDto?.datetime) > new Date();
     }
 }
