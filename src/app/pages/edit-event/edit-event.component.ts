b2614d16507743ad09e4928a65f10d98ba7e7340
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-07-26 18:53:59 +0200

    Added location search via mapbox-geocoding.

--- src/app/pages/edit-event/edit-event.component.ts
+++ src/app/pages/edit-event/edit-event.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {EventDto} from "../../core/dto/event-dto";
 import {EventService} from "../../core/services/event.service";
 import {AuthenticationService} from "../../core/services/authentication.service";
 import {CategoryDto} from "../../core/dto/category-dto";
 import {CategoryService} from "../../core/services/category.service";
+import mapboxgl from 'mapbox-gl'
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 @Component({
   selector: 'app-edit-event',
   templateUrl: './edit-event.component.html',
   styleUrls: ['./edit-event.component.css']
 })
-export class EditEventComponent {
+export class EditEventComponent implements AfterViewInit {
     editEvent: FormGroup;
     categories: Array<CategoryDto>;
+    latitude: number;
+    longitude: number;
+    place_name: string;
 
     constructor(private _formBuilder: FormBuilder,
                 private _eventService: EventService,
@@ -23,11 +28,33 @@ export class EditEventComponent {
         this.editEvent = _formBuilder.group({
             title: [''],
             datetime: [],
-            place: [''],
             price: [0],
             categories: [],
             description: ['']
         })
+    }
+
+    ngAfterViewInit() {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoid2ViY3ZkIiwiYSI6ImNsa2ZwYm56MDA4ZzIzc3NleTMwdnhsMWIifQ.sU_r9FMc4zD1FAlNTvzppw';
+        const geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            types: 'country,region,place,postcode,locality,neighborhood,address,poi'
+        });
+
+        geocoder.addTo('#geocoder');
+
+        const results = document.getElementById('result');
+
+        geocoder.on('result', (e) => {
+            const coordinates = e.result.center;
+            this.longitude = coordinates[0];
+            this.latitude = coordinates[1];
+            this.place_name = e.result.text.split(',')[0].trim();
+        });
+
+        geocoder.on('clear', () => {
+            results.innerText = '';
+        });
     }
 
     private addFather() {
@@ -49,7 +76,8 @@ export class EditEventComponent {
         this.addFather()
         const event: EventDto = {
             ...this.editEvent.value,
-            coordinates: "10.00000 10.000000",
+            place: this.place_name,
+            coordinates: this.longitude + ", " + this.latitude,
             organiser: AuthenticationService.getAppUser
         }
         this._eventService.save(event).subscribe()
