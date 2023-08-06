30212554385b84106c7987b0651eb58557b82224
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-08-06 20:52:20 +0200

    Adjusted saving location logic in Search Service, location now gets saved on db

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,32 +1,56 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable} from '@angular/core'
+import {AuthenticationService} from './authentication.service'
+import {UserService} from './user.service'
 
 @Injectable({providedIn: "root"})
 export class SearchService {
-    private _searchLocation: string = "Roma";
-    private _searchLatitude: string = "12.492650148954052";
-    private _searchLongitude: string = "41.890353287010065";
+    private _defaultLocation: string = "Roma";
+    private _defaultLatitude: string = "12.492650148954052";
+    private _defaultLongitude: string = "41.890353287010065";
 
-    get searchLocation(): string {
-        return this._searchLocation;
+    setNewLocation(place: string, latitude: string, longitude: string) {
+        this._defaultLocation = place
+        this._defaultLatitude = latitude
+        this._defaultLongitude = longitude
+
+        this._saveLocation()
     }
 
-    set searchLocation(value: string) {
-        this._searchLocation = value;
+    private _saveLocation () {
+        inject(UserService).save({
+            ...AuthenticationService.getAppUser,
+            searchLocation: this.searchLocation,
+            searchLongitude: this.searchLongitude,
+            searchLatitude: this.searchLatitude
+        })
+    }
+
+    get searchLocation(): string {
+        if (AuthenticationService.isLogged()) {
+            if (!!AuthenticationService.getAppUser.searchLocation)
+                return AuthenticationService.getAppUser.searchLocation
+            this._saveLocation()
+        }
+
+        return this._defaultLocation;
     }
 
     get searchLatitude(): string {
-        return this._searchLatitude;
-    }
+        if (AuthenticationService.isLogged()) {
+            if (!!AuthenticationService.getAppUser.searchLatitude)
+                return AuthenticationService.getAppUser.searchLatitude
+            this._saveLocation()
+        }
 
-    set searchLatitude(value: string) {
-        this._searchLatitude = value;
+        return this._defaultLatitude;
     }
 
     get searchLongitude(): string {
-        return this._searchLongitude;
-    }
-
-    set searchLongitude(value: string) {
-        this._searchLongitude = value;
+        if (AuthenticationService.isLogged()) {
+            if (!!AuthenticationService.getAppUser.searchLongitude)
+                return AuthenticationService.getAppUser.searchLongitude
+            this._saveLocation()
+        }
+        return this._defaultLongitude;
     }
 }
