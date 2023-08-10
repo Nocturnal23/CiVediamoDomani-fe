00a7858ec73535859f1d661cdc30d1d98cecd732
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-08-10 21:10:48 +0200

    Added mapbox field in dialog-add-category

--- src/app/layout/dialog-add-category/dialog-add-category.component.ts
+++ src/app/layout/dialog-add-category/dialog-add-category.component.ts
@@ -1,24 +1,64 @@
-import {Component, Inject} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import mapboxgl from 'mapbox-gl'
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
+enum InputType {
+    Text = 'text',
+    Map = 'map'
+}
 @Component({
-  selector: 'app-dialog-add-category',
-  templateUrl: './dialog-add-category.component.html',
-  styleUrls: ['./dialog-add-category.component.css']
+    selector: 'app-dialog-add-category',
+    templateUrl: './dialog-add-category.component.html',
+    styleUrls: ['./dialog-add-category.component.css']
 })
-export class DialogAddCategoryComponent {
-  newCategory: FormGroup
-  newName: string
 
-  constructor(private formBuilder: FormBuilder,
-              @Inject(MAT_DIALOG_DATA) public data: { dialogTitle: string }) {
-    this.newCategory = formBuilder.group({
-      name: ['']
-    })
-  }
+export class DialogAddCategoryComponent implements AfterViewInit{
+    newCategory: FormGroup
+    newName: string
+    latitude: number
+    longitude: number
+    place_name: string
+    selectedPosition
+    protected readonly InputType = InputType;
 
-  createCat() {
-    this.newName = this.newCategory.value.name
-  }
+
+    constructor(private formBuilder: FormBuilder,
+                @Inject(MAT_DIALOG_DATA) public data: { dialogTitle: string, placeholderText: string, inputType: InputType }) {
+        this.newCategory = formBuilder.group({
+            name: ['']
+        })
+    }
+
+    createCat() {
+        this.newName = this.newCategory.value.name
+    }
+
+    ngAfterViewInit() {
+        if (this.data.inputType === InputType.Map) {
+            mapboxgl.accessToken = 'pk.eyJ1Ijoid2ViY3ZkIiwiYSI6ImNsa2ZwYm56MDA4ZzIzc3NleTMwdnhsMWIifQ.sU_r9FMc4zD1FAlNTvzppw';
+            const geocoder = new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                types: 'country,region,place'
+            });
+
+            geocoder.addTo('#geocoder');
+
+            const results = document.getElementById('result');
+
+            geocoder.on('result', (e) => {
+                const coordinates = e.result.center;
+                this.longitude = coordinates[0];
+                this.latitude = coordinates[1];
+                this.place_name = e.result.text.split(',')[0].trim();
+
+                this.selectedPosition = {lon: this.longitude, lat: this.latitude, place: this.place_name}
+            });
+
+            geocoder.on('clear', () => {
+                results.innerText = '';
+            });
+        }
+    }
 }
