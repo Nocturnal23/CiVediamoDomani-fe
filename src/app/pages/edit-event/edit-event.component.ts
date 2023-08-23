6119f2ccae8472f3685fc993d1573b9cf0b0b40d
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-08-23 14:23:29 +0200

    Changed method of choosing cities for edit-event.

--- src/app/pages/edit-event/edit-event.component.ts
+++ src/app/pages/edit-event/edit-event.component.ts
@@ -8,23 +8,27 @@ import {CategoryService} from "../../core/services/category.service";
 import mapboxgl from 'mapbox-gl'
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import {Router} from "@angular/router";
+import {DialogAddCategoryComponent} from "../../layout/dialog-add-category/dialog-add-category.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-edit-event',
   templateUrl: './edit-event.component.html',
   styleUrls: ['./edit-event.component.css']
 })
-export class EditEventComponent implements AfterViewInit {
+export class EditEventComponent {
     editEvent: FormGroup;
     categories: Array<CategoryDto>;
     latitude: number;
     longitude: number;
     place_name: string;
+    test = "ROMA"
 
     constructor(private _formBuilder: FormBuilder,
                 private _eventService: EventService,
                 private _categoryService: CategoryService,
-                private _router: Router) {
+                private _router: Router,
+                private _dialog: MatDialog) {
         this._categoryService.filter({pageSize: 50}).subscribe( res => this.categories = res.content )
 
         this.editEvent = _formBuilder.group({
@@ -36,30 +40,30 @@ export class EditEventComponent implements AfterViewInit {
         })
     }
 
-    ngAfterViewInit() {
-        mapboxgl.accessToken = 'pk.eyJ1Ijoid2ViY3ZkIiwiYSI6ImNsa2ZwYm56MDA4ZzIzc3NleTMwdnhsMWIifQ.sU_r9FMc4zD1FAlNTvzppw';
-        const geocoder = new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            types: 'country,region,postcode,district,place,locality,neighborhood,address,poi'
-        });
-
-        geocoder.addTo('#geocoder');
-
-        const results = document.getElementById('result');
-
-        geocoder.on('result', (e) => {
-            const coordinates = e.result.center;
-            this.longitude = coordinates[0];
-            this.latitude = coordinates[1];
-            this.place_name = e.result.text.split(',')[0].trim();
-
-            const city = e.result.place_name.split(',')[e.result.place_name.split(',').length - 3].trim();
-        });
-
-        geocoder.on('clear', () => {
-            results.innerText = '';
-        });
-    }
+    // ngAfterViewInit() {
+    //     mapboxgl.accessToken = 'pk.eyJ1Ijoid2ViY3ZkIiwiYSI6ImNsa2ZwYm56MDA4ZzIzc3NleTMwdnhsMWIifQ.sU_r9FMc4zD1FAlNTvzppw';
+    //     const geocoder = new MapboxGeocoder({
+    //         accessToken: mapboxgl.accessToken,
+    //         types: 'country,region,postcode,district,place,locality,neighborhood,address,poi'
+    //     });
+    //
+    //     geocoder.addTo('#geocoder');
+    //
+    //     const results = document.getElementById('result');
+    //
+    //     geocoder.on('result', (e) => {
+    //         const coordinates = e.result.center;
+    //         this.longitude = coordinates[0];
+    //         this.latitude = coordinates[1];
+    //         this.place_name = e.result.text.split(',')[0].trim();
+    //
+    //         const city = e.result.place_name.split(',')[e.result.place_name.split(',').length - 3].trim();
+    //     });
+    //
+    //     geocoder.on('clear', () => {
+    //         results.innerText = '';
+    //     });
+    // }
 
     private addFather() {
         const childCategories: CategoryDto[] = this.editEvent.value.categories;
@@ -85,5 +89,21 @@ export class EditEventComponent implements AfterViewInit {
             organiser: AuthenticationService.getAppUser
         }
         this._eventService.save(event).subscribe( event => this._router.navigate(['infoevent/'+event.url]) )
+    }
+
+    changeLocation(title:string, placeholder:string) {
+        let dialogContent = this._dialog.open(DialogAddCategoryComponent, {
+            data: {
+                dialogTitle: title,
+                placeholder: placeholder,
+                inputType: 'map'
+            }
+        })
+
+        dialogContent.afterClosed().subscribe(res => {
+            this.place_name = dialogContent.componentInstance.selectedPosition.place
+            this.latitude = dialogContent.componentInstance.selectedPosition.lat
+            this.longitude = dialogContent.componentInstance.selectedPosition.lon
+        })
     }
 }
